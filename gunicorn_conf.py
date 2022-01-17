e7dc790453c23aa9d2110977f307962c2b7b71f4
cccnrc <ec3408@cumc.columbia.edu>
2022-01-17 12:42:58 +0100

    diagnosticator-server-tutorial v0.9

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -1,6 +1,6 @@
 bind = ':8001'
 worker_class = 'sync'
 loglevel = 'debug'
-accesslog = '/home/ec2-user/diagnosticator-server-tutorial/logs/diagnosticator_access.log'
+accesslog = '/home/ubuntu/diagnosticator-server-tutorial/logs/diagnosticator_access.log'
 acceslogformat ="%(h)s %(l)s %(u)s %(t)s %(r)s %(s)s %(b)s %(f)s %(a)s"
-errorlog =  '/home/ec2-user/diagnosticator-server-tutorial/logs/diagnosticator_error.log'
+errorlog =  '/home/ubuntu/diagnosticator-server-tutorial/logs/diagnosticator_error.log'
